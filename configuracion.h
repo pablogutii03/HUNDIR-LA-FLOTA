2d1f9f4bb77bcd716edca013393729dc8d31b075
usulkirck <gutierrezvillarin@gmail.com>
2025-03-17 16:15:01 +0100

    Add files via upload

---
+++ configuracion.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "estructuras.h"
+
+
+//funciones
+void introducir_datos();
+void mostrar_resumen();
+void borrar_configuracion();
+void guardar_datos();
+void cargar();
+
+
