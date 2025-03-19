76e7b8ded786c44332196d2988d228937f053793
usulkirck <gutierrezvillarin@gmail.com>
2025-03-19 12:12:01 +0100

    Update configuracion.h

--- configuracion.h
+++ configuracion.h
@@ -1,14 +1,15 @@
+
+#ifndef CONFIGURACION_H
+#define CONFIGURACION_H
 #include <stdio.h>
-#include <stdlib.h>
-
-#include "estructuras.h"
 
 
-//funciones
+void inicio_config();
 void introducir_datos();
 void mostrar_resumen();
 void borrar_configuracion();
-void guardar_datos();
-void cargar();
 
+
+
+#endif
 
