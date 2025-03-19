b14c146430e9c08370f47c9beb98d54a86af9c0f
JuanjoPM <juanjopma333@gmail.com>
2025-03-19 16:24:40 +0100

    Actualizar el fichero de cabecera
    Objetivo principal: adecuarse a los cambios en el codigo del módulo

--- tablero.h
+++ tablero.h
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>         //importa librerías estandar
 
-#include  "estructuras.h" 
+#include  "estructuras.h"
 #include  "configuracion.h"
 
 //funciones a exportar
-void reiniciar_tablero(FILE * jugador)  //inicializa los tableros para ser configurados de nuevo
-void introducir_tablero(FILE * jugador) //funcion para que el usuario al cual le corresponde el fichero que recibe como puntero introduzca la posicion de sus barcos
-char *obtener_dato_casilla(char x, int y) //devuelve el valor que contiene la posicion especificada en el formato que entrega el usuario
+
+void reiniciar_tablero(char **tablero)  //inicializa los tableros con manteniendo la matriz proporcionada//
+
+//precondicion: op_tab debe ser 1 (en caso de querer crear el tablero jugador y desplegar la interfaz para que este introduzca sus barcos) o 2 (solo crea e inicializa la matriz para el oponente)
+char** introducir_tablero(int op_tab, int n) //funcion para inicializar los tableros y que el usuario introduzca la posicion de sus barcos en caso de seleccionar op_tab = 1
